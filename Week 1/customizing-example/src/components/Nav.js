e0ccf2e24cf9b9a7f9173da7a1784d850685227f
hyz <yaozhihan70@gmail.com>
2024-04-29 22:53:44 +0800

    update

---
+++ Week 1/customizing-example/src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav className="main-nav">
+            <u1>
+                <li>Home</li>
+                <li>Articles</li>
+                <li>About</li>
+                <li>Contact</li>
+            </u1>
+        </nav>
+    );
+};
+
+export default Nav;
